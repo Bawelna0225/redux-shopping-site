be4008c6d07fbf9af2c470a56a95d0e595c2fa98
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-04-04 15:24:54 +0200

    more navbar items & react icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 export default function Navbar() {
-  return (
-    <nav className='navbar'>Navbar</nav>
-  )
+	return (
+		<nav className="navbar">
+			<div className="logo">LOGO</div>
+			<ul>
+				<li>
+					<select>
+						<option>$</option>
+						<option>₤</option>
+						<option>€</option>
+						<option>¥</option>
+					</select>
+				</li>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/products">Products</Link>
+				</li>
+        <li><button>Cart <AiOutlineShoppingCart></AiOutlineShoppingCart></button></li>
+			</ul>
+		</nav>
+	)
 }
