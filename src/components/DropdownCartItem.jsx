d908b233c08981ca302e0a64a6cd7c708f4e6e0a
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-04-06 15:22:15 +0200

    dropdown cart item component

---
+++ src/components/DropdownCartItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DropdownCartItem() {
+  return (
+    <div>DropdownCartItem</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,5 @@ import { useSelector } from 'react-redux'
 
 export default function Home() {
 	const currency = useSelector((state) => state.currency.currency)
-  console.log(currency)
-	return <section>{currency[0]}</section>
+	return <section><h1>current currency: {currency[0]}</h1></section>
 }
